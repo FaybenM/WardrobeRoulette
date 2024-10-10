f3c791ec0c0234d25fe45eca80ca65186f35274b
Fayben <faybenm98@gmail.com>
2024-10-10 03:58:03 -0400

    Initial Commit

---
+++ WardrobeRoulette/WardrobeRouletteApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardrobeRouletteApp.swift
+//  WardrobeRoulette
+//
+//  Created by Fayben on 10/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WardrobeRouletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WardrobeRouletteTests/WardrobeRouletteTests.swift
@@ -0,0 +1,17 @@
+//
+//  WardrobeRouletteTests.swift
+//  WardrobeRouletteTests
+//
+//  Created by Fayben on 10/10/24.
+//
+
+import Testing
+@testable import WardrobeRoulette
+
+struct WardrobeRouletteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
